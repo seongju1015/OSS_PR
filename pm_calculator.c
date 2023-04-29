e2eb6a3fb0a97d6c559e725babd87306aa5fcbb0
zoey <yoonzoeym27@gmail.com>
2023-04-29 21:20:53 +0900

    minus 함수 구현

--- pm_calculator.c
+++ pm_calculator.c
@@ -5,13 +5,23 @@ int plus(int num1, int num2){
     result = num1 + num2;
     return result;
 }
+
+int minus(int num1, int num2) {
+    int result;
+    result = num1 - num2;
+    return result;
+}
 int main(){
     int n1, n2;
     int result_1;
+    int result_2;
+
     printf("두 숫자를 입력\n");
     scanf("%d %d",&n1, &n2);
     result_1 = plus(n1,n2);
+    result_2 = minus(n1,n2);
     printf("덧셈 결과는: %d",result_1);
+    printf("\n뺄셈 결과는: %d\n",result_2);
 
     return 0;
 }
